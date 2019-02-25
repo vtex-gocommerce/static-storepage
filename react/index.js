cdd731bea6dfac766ea127989ae1fab412591b1c
Gustavo Rosolem <guspace92@hotmail.com>
2019-02-25 10:12:02 -0300

    Initial commit

---
+++ react/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const bootstrap = () => {
+  return (
+    <div className="vtex-account helvetica flex justify-around">
+      Sobre nós
+    </div>
+  )
+}
+
+export default bootstrap
